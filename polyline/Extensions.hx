a6184e74a73b047fee4e66e1c43da8a3748fe766
Jeremy Faivre <contact@jeremyfa.com>
2021-10-06 17:48:15 +0200

    Fix unsafeGet() generating _hx_isInstanceOf() calls on cpp target

--- polyline/Extensions.hx
+++ polyline/Extensions.hx
@@ -9,7 +9,13 @@ class Extensions {
         if (index < 0 || index >= array.length) throw 'Invalid unsafeGet: index=$index length=${array.length}';
 #end
 #if cpp
+        #if app_cpp_nativearray_unsafe
         return cpp.NativeArray.unsafeGet(array, index);
+        #else
+        return untyped array.__unsafe_get(index);
+        #end
+#elseif cs
+        return cast untyped __cs__('{0}.__a[{1}]', array, index);
 #else
         return array[index];
 #end
@@ -20,7 +26,13 @@ class Extensions {
         if (index < 0 || index >= array.length) throw 'Invalid unsafeSet: index=$index length=${array.length}';
 #end
 #if cpp
+        #if app_cpp_nativearray_unsafe
         cpp.NativeArray.unsafeSet(array, index, value);
+        #else
+        untyped array.__unsafe_set(index, value);
+        #end
+#elseif cs
+        return cast untyped __cs__('{0}.__a[{1}] = {2}', array, index, value);
 #else
         array[index] = value;
 #end
